49100afc83e137804feb1224b8dc4b3530e11764
Kevin Julien <kevin.julien@canal-plus.com>
2019-07-04 17:27:08 +0200

    Add solution 4

--- Euler_004.py
+++ Euler_004.py
@@ -1,5 +1,15 @@
+def isPalindrome(n):
+    return (int(str(n)[::-1]) == n)
+
 def euler004():
-    print("euler004")
+    res = 42
+    for x in range(100, 1000):
+        for y in range(100, 1000):
+            if isPalindrome(x * y):
+                if (x * y > res):
+                    res = x * y
+
+    print(res)
 
 if __name__ == "__main__":
     euler004()
