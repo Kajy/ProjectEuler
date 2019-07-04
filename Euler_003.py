87cb1599c4993f6e19e95ea8d66a99f1e483664f
Kevin Julien <kevin.julien@canal-plus.com>
2019-07-04 17:09:02 +0200

    opt 003

--- Euler_003.py
+++ Euler_003.py
@@ -1,21 +1,10 @@
 import math
 
-def isPrime(n):
-    if n % 2 == 0:
-        return n == 2
-    else:
-        for x in range(3, round(math.sqrt(n)), 2):
-            if n % x == 0:
-                return 0
-        return 1
-
 def euler003(n):
     for i in range(2, round(math.sqrt(n))):
-        if isPrime(i):
             if n % i == 0:
                 euler003(n / i)
                 break
-
     print(round(n))
     exit()
 
---
+++ Euler_004.py
@@ -0,0 +1,5 @@
+def euler004():
+    print("euler004")
+
+if __name__ == "__main__":
+    euler004()
